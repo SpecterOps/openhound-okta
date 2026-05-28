9c6b0e8da0a02ab398bbe856c75f58312f362ed2
Joey Dreijer <jdreijer@specterops.io>
2026-05-28 16:06:20 +0200

    Make additional Application fields optional

--- src/openhound_okta/models/application.py
+++ src/openhound_okta/models/application.py
@@ -47,9 +47,9 @@ class OauthClientSettings(BaseModel):
     client_uri: str | None = None
     response_types: list[str] = Field(default_factory=list)
     grant_types: list[str] = Field(default_factory=list)
-    application_type: str
-    issuer_mode: str
-    consent_method: str
+    application_type: str | None = None
+    issuer_mode: str | None = None
+    consent_method: str | None = None
     jwks: OauthKeys | None = None
 
 
