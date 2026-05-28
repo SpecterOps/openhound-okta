7dc43cdc7fe0974e6450954be9ebe54e86cbebf6
Joey Dreijer <jdreijer@specterops.io>
2026-05-28 16:15:26 +0200

    Set additional optional fields

--- src/openhound_okta/models/application.py
+++ src/openhound_okta/models/application.py
@@ -30,10 +30,10 @@ class ApplicationProperties(OktaNodeProperties):
 
 class JWK(BaseModel):
     id: str
-    kid: str
+    kid: str | None = None
     alf: str | None = None
-    use: str
-    n: str
+    use: str | None = None
+    n: str | None = None
     status: str
     last_updated: datetime | None = Field(default=None, alias="lastUpdated")
     created: datetime | None = None
@@ -65,9 +65,9 @@ class Settings(BaseModel):
 
 class OAuthCredential(BaseModel):
     auto_key_rotation: bool | None = Field(default=None, alias="autoKeyRotation")
-    client_id: str
+    client_id: str | None = None
     token_endpoint_auth_method: str
-    pkce_required: bool
+    pkce_required: bool | None = None
 
 
 class Credentials(BaseModel):
